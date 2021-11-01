f2b0eed634e3ca26d6696dc6314726525f461f9a
mesu-ai <mesu.s.ss@gmail.com>
2021-11-01 22:22:52 +0600

    all updated

---
+++ README.md
@@ -0,0 +1,29 @@
+# Welcome to- Tour-Explorer server
+
+
+Live Client Site Firebase Hosting-
+## Live site : https://tour-explorer.web.app/
+
+
+# Site Tools:
+* HTML
+* CSS
+* Bootsrap
+* Java-Script
+* React JS
+* node
+* React Router
+* React Hook Form
+* Axios
+* Fontawesome Icon
+* Firebase
+
+
+# Site Description:
+* It is a Tourism website.
+* Find all tour relatedd Service.
+* Helpful for booking,fees payment.
+* Get 24/7 emergency suppport.
+* Not harmful or spaming site.
+
+
--- index.js
+++ index.js
@@ -20,10 +20,11 @@ async function run() {
       await client.connect();
       const database = client.db("tour-explorer");
       const tourCollection = database.collection("tourPackage");
+      const bookingCollection=database.collection("booking");
+      
       console.log('connect to db...');
 
-    
-
+   
       
       // get api
 
@@ -45,13 +46,12 @@ async function run() {
       });
 
 
-
       // post api
       app.post('/tourpackages',async(req,res)=>{
        
         const addtourPackage=req.body;
         const result =await tourCollection.insertOne(addtourPackage);
-        console.log(`Package Inserted: ${result.insertedId}`)
+        console.log(`Package Inserted: ${result.insertedId}`);
         res.json(result);
 
       });
@@ -93,7 +93,93 @@ async function run() {
         const result = await tourCollection.updateOne(filter, updateDoc, options);
         res.json(result);
 
-       })
+       });
+
+
+      //  get booking
+
+      app.get('/booking',async(req,res)=>{
+        const cursor=bookingCollection.find({});
+        const result=await cursor.toArray();
+        res.send(result);
+
+      });
+
+      // get selected api
+
+      app.get('/booking/:id',async(req,res)=>{
+        const id=req.params.id;
+        const quary={_id:ObjectId(id)};
+        const result= await bookingCollection.findOne(quary);
+        res.send(result);
+
+
+      });
+
+      //post booking
+
+      app.post('/booking',async(req,res)=>{
+        const addToBooking=req.body;
+        const result=await bookingCollection.insertOne(addToBooking);
+        console.log(`Package Inserted: ${result.insertedId}`)
+        res.json(result);
+
+      });
+
+      // booking api delete
+      app.delete('/booking/:id',async(req,res)=>{
+        const id=req.params.id;
+        const quary={_id:ObjectId(id)};
+        const result=await bookingCollection.deleteOne(quary);
+        res.json(result);
+
+      });
+
+
+      //update api
+      app.put('/booking/:id',async(req,res)=>{
+        const id=req.params.id;
+        const updatedBooking=req.body;
+        const filter={_id:ObjectId(id)};
+        const options = { upsert: true };
+        const updateDoc = {
+         $set: {
+           
+           name:updatedBooking.neme,
+           email:updatedBooking.email,
+           age:updatedBooking.age,
+           phone:updatedBooking.phone,
+           tourpack:updatedBooking.tourpack,
+           tourprice:updatedBooking.tourprice,
+           date:updatedBooking.date,
+           totalprice:updatedBooking.totalprice,
+           bookingStatus:updatedBooking.bookingStatus,
+            
+   
+         },
+       };
+       const result = await bookingCollection.updateOne(filter, updateDoc, options);
+       res.json(result);
+
+      });
+
+      app.put('/booking/:id',async(req,res)=>{
+        const id=req.params.id;
+        const updateStatus=req.body.bookingStatus;
+        const filter={_id:ObjectId(id)};
+        const options = { upsert: true };
+        const updateDoc = {
+         $set: {
+           bookingStatus:updateStatus,
+   
+         },
+       };
+       const result = await bookingCollection.updateOne(filter, updateDoc, options);
+       res.json(result);
+
+      });
+
+
       
 
     } finally {
